42cf89ca9afd3fb551dbbe98392d0b05504e02e7
Pavel <psobolev71@gmail.com>
2021-06-10 22:59:12 +0300

    gitignore + homework2_1

---
+++ homework_2/homework_2_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите максимальную температуру за сутки.");
+            double MaxTemp = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите минимальную температуру за сутки.");
+            double MinTemp = Convert.ToDouble(Console.ReadLine());
+
+            double SumTemp = MaxTemp + MinTemp;
+            double MidTemp = (SumTemp / 2);
+
+            Console.WriteLine($"Средняя температура за сутки равна {MidTemp}");
+
+            Console.ReadLine();
+        }
+    }
+}
